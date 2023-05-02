75cc94c46682afba90981014e810614da76075d0
Matthew Ruse <mr664@uowmail.edu.au>
2023-05-02 17:57:47 +1000

    simulate error

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 app.listen(PORT, () => {
