f348e8e52e2f164b3117ff54d6fbd22b166294f6
Matthew Ruse <mr664@uowmail.edu.au>
2023-05-02 14:39:59 +1000

    add new test

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('when navigate to ivysaur, check chlorophyll and overgrow are present', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
+  })
 })
