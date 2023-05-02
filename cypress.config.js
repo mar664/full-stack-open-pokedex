ffa5b6b4a6c89950f1534f65751d4efb1b11e424
Matthew Ruse <mr664@uowmail.edu.au>
2023-05-02 13:36:29 +1000

    fix cypress eslint

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on, config) { // eslint-disable-next-line no-unused-vars
       // implement node event listeners here
     },
   },
-});
+})
